6f9f312409546ad3237a8eda94bf98501afaec54
Alexander Zhivkov <sashko.zhivkov@gmail.com>
2022-08-03 11:13:24 +0300

    Project initialization

---
+++ formula-one-client-tinqin/src/main/java/com/tinqin/academy/formulaoneclienttinqin/FormulaOneClientTinqinApplication.java
@@ -0,0 +1,13 @@
+package com.tinqin.academy.formulaoneclienttinqin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaOneClientTinqinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormulaOneClientTinqinApplication.class, args);
+    }
+
+}
---
+++ formula-one-client-tinqin/src/test/java/com/tinqin/academy/formulaoneclienttinqin/FormulaOneClientTinqinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinqin.academy.formulaoneclienttinqin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaOneClientTinqinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
